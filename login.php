b92c7a36314dea1e6ad21da9ebe84cc8ec9d3285
Maheshbhatta <34996801+Maheshbhatta@users.noreply.github.com>
2018-01-01 14:43:42 +0545

    Add files via upload

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Admin Login</title>
+</head>
+<body>
+<form method="post" action="processing/loginchechk.php">
+Username<input type="text" name="username">
+Password<input type="password" name="password">
+<input type="submit" name="login" value="Login">
+
+</form>
+</body>
+</html>
